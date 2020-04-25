c27d3a25a3ff17e206af4720cb097f52fa85710c
yinqicheng <785128850@qq.com>
2020-04-25 21:13:28 +0800

    init

--- ssm/src/main/java/com/syscom/ssm/controller/TestController.java
+++ ssm/src/main/java/com/syscom/ssm/controller/TestController.java
@@ -1,13 +1,13 @@
 package com.syscom.ssm.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Objects;
 
 /**
  * @author yinqicheng
@@ -27,4 +27,45 @@ public class TestController {
     public String restful(@PathVariable("id") String id) {
         return id;
     }
+
+
+    public static void main(String[] args) {
+        String password = "000000";
+        String salt1 = "Rita123321";
+        String salt2 = "1234567Rossweisse";
+        int half = password.length() >> 1;
+
+        String str = salt1 + password.substring(0, half) + salt2 + password.substring(half);
+        str = str + str.hashCode();
+        System.out.println(str.length());
+        System.out.println("scramble = " + scramble(str));
+    }
+
+
+    public static String scramble(String s) {
+        Objects.requireNonNull(s);
+        char[] chars = s.toCharArray();
+        return new String(scramble(chars, 0, chars.length));
+    }
+
+    public static char[] scramble(char[] chars, int start, int end) {
+        int mid = (start + end) >> 1;
+        if (mid == start || mid == end) {
+            return new char[]{chars[mid]};
+        }else {
+            char[] left = scramble(chars, start, mid);
+            char[] right = scramble(chars, mid, end);
+            char[] newChars = new char[left.length + right.length];
+            if (left.length == 1 || right.length == 1) {
+                System.arraycopy(right, 0, newChars, 0, right.length);
+                System.arraycopy(left, 0, newChars, right.length, left.length);
+            } else {
+                System.arraycopy(left, 0, newChars, 0, left.length);
+                System.arraycopy(right, 0, newChars, left.length, right.length);
+            }
+            return newChars;
+        }
+
+    }
+
 }
